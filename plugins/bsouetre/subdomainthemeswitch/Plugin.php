b18775489115843862f68d9c9319a290cf42dd78
rgsone <rgs@rgsone.com>
2017-03-03 12:03:53 +0100

    add SubDomainThemeSwitch plugin

---
+++ plugins/bsouetre/subdomainthemeswitch/Plugin.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace BSouetre\SubDomainThemeSwitch;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Request;
+use System\Classes\PluginBase;
+
+/**
+ * Class Plugin
+ * @package BSouetre\SubDomainThemeSwitch
+ */
+class Plugin extends PluginBase
+{
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'SubDomainThemeSwitch',
+            'description' => 'Bind theme to current subdomain',
+            'author' => 'rgsone',
+            'icon' => 'icon-leaf'
+        ];
+    }
+
+    public function boot()
+    {
+    	# if backend > skip
+		if ( App::runningInBackend() ) return;
+
+		$currentHost = Request::getHost();
+		$allowedDomains = Config::get( 'bsouetre.subdomainthemeswitch::allowed_domains' );
+		$subdomains = Config::get( 'bsouetre.subdomainthemeswitch::subdomains' );
+		$matchDomain = false;
+
+		# check if current host match allowed domain
+		foreach ( $allowedDomains as $domain )
+		{
+			if ( preg_match( '/' . $domain . '$/i', $currentHost ) )
+			{
+				$matchDomain = true;
+				break;
+			}
+		}
+
+		# no match > skip
+		if ( !$matchDomain ) return;
+
+		# check if subdomain match
+		Event::listen( 'cms.theme.getActiveTheme', function () use ( $subdomains, $currentHost ) {
+
+			foreach ( $subdomains as $subdomain => $themeName )
+			{
+				# check if match defined subdomain
+				if ( preg_match( '/' . $subdomain . '\./i', $currentHost ) ) return $themeName;
+			}
+
+		});
+    }
+}
---
+++ plugins/bsouetre/subdomainthemeswitch/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'subdomains' => [
+		'shop' => 'bsouetre-shop',
+		'galerie' => 'bsouetre-galerie'
+	],
+	'allowed_domains' => [
+		'brunosouetre.net',
+		'bsouetre.dev'
+	]
+];
