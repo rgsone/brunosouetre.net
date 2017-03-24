0b2ee525e0af48bb2ef58878db403c0c696e57cd
rgsone <rgs@rgsone.com>
2017-03-24 18:43:35 +0100

    add new fields in global site settings + reorder layout

--- plugins/bsouetre/site/models/Setting.php
+++ plugins/bsouetre/site/models/Setting.php
@@ -17,12 +17,21 @@ class Setting extends Model
 	public $settingsCode = 'bsouetre_site_settings';
 	public $settingsFields = 'fields.yaml';
 
+	public $attachOne = [
+		'highlighted_item_img' => 'System\Models\File'
+	];
+
 	public $rules = [
-		'email' => 'required|email'
+		'email' => 'required|email',
+		'highlighted_item_desc' => 'max:1024',
+		'highlighted_item_url' => 'max:512|url'
 	];
 
 	public $customMessages = [
 		'email.required' => 'L\'email n\'est pas renseigné.',
-		'email.email' => 'L\'email n\'est pas valide.'
+		'email.email' => 'L\'email n\'est pas valide.',
+		'highlighted_item_desc.max' => 'La description de l\'article mise en avant est trop longue (1024 caractéres maximum).',
+		'highlighted_item_url.max' => 'L\'URL de l\'article mise en avant est trop longue (1024 caractéres maximum).',
+		'highlighted_item_url.url' => 'L\'URL de l\'article mise en avant n\'est pas valide.'
 	];
 }
