8942b2d5c322c284d66078691dab9bd7127ba186
rgsone <rgs@rgsone.com>
2017-03-26 00:00:28 +0100

    implement page about

--- plugins/bsouetre/site/components/AboutPage.php
+++ plugins/bsouetre/site/components/AboutPage.php
@@ -2,7 +2,8 @@
 
 namespace BSouetre\Site\Components;
 
-use BSouetre\Site\Models\About;
+use BSouetre\Site\Models\Link;
+use BSouetre\Site\Models\Setting;
 use Cms\Classes\ComponentBase;
 
 /**
@@ -26,12 +27,13 @@ class AboutPage extends ComponentBase
 
 	public function onRun()
 	{
-		$this->page[ 'content' ] = About::get( 'content', '' );
+		$this->page[ 'content' ] = Setting::get( 'about_content' );
+		$this->page[ 'links' ] = Link::orderBy( 'name', 'asc' )->get();
 		$this->page[ 'nav' ] = [
-			'home' => [ 'title' => 'Accueil', 'url' => $this->controller->pageUrl( 'home' ) ],
-			'archives' => [ 'title' => 'Archives', 'url' => $this->controller->pageUrl( 'archives' ) ],
-			'about' => [ 'title' => 'À Propos', 'url' => $this->controller->pageUrl( 'about' ) ],
-			'contact' => [ 'title' => 'Contact', 'url' => $this->controller->pageUrl( 'contact' ) ]
+			'home' => [ 'url' => $this->controller->pageUrl( 'home' ) ],
+			'archives' => [ 'url' => $this->controller->pageUrl( 'archives' ) ],
+			'about' => [ 'url' => $this->controller->pageUrl( 'about' ) ],
+			'contact' => [ 'url' => $this->controller->pageUrl( 'contact' ) ]
 		];
 	}
 }
