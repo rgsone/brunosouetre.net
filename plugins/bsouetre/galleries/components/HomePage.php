c0b844ece60ebdfc13e4e45770557be0ea11570a
rgsone <rgs@rgsone.com>
2017-04-08 22:08:18 +0200

    update Galleries HomePage component

--- plugins/bsouetre/galleries/components/HomePage.php
+++ plugins/bsouetre/galleries/components/HomePage.php
@@ -2,7 +2,10 @@
 
 namespace BSouetre\Galleries\Components;
 
+use BSouetre\Galleries\Classes\GalleryList;
+use BSouetre\Galleries\Classes\Router;
 use Cms\Classes\ComponentBase;
+use October\Rain\Support\Facades\Config;
 
 /**
  * Class HomePage
@@ -20,6 +23,13 @@ class HomePage extends ComponentBase
 
 	public function onRun()
 	{
-		$this->page[ 'galleries' ] = [];
+		$router = new Router( Config::get( 'bsouetre.galleries::path' ) );
+		$galleryList = new GalleryList( $router );
+		$galleryList->setCacheTtl( Config::get( 'bsouetre.galleries::cacheTTL.galleries', 1 ) );
+		$galleries = $galleryList->listGalleries( Config::get( 'app.debug', false ) );
+
+		$this->page[ 'galleries' ] = $galleries->filter( function( $item ) {
+			return ( $item->published == true && $item->listed == true );
+		});
 	}
 }
