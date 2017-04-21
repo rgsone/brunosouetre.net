cb659f95dfc6023d37cf7eeb0bc04a33c411d739
rgsone <rgs@rgsone.com>
2017-04-21 19:31:32 +0200

    Theme site : add assets src

---
+++ themes/bsouetre/assets/src/index.js
@@ -0,0 +1,9 @@
+/* CSS */
+import './style/app.scss';
+/* JS */
+import jump from 'jump.js';
+
+// Hello !
+
+console.log( '%c /> brunosouetre.net ', 'background: #333; color: #fc5454' );
+console.log( '%c /> handmade by rgsone.com ', 'background: #333; color: #fc5454' );
